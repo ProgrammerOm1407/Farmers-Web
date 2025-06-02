833cf7f713f82cd000884304dde1505909cf062b
omdchavan2020 <omdchavan2020@gmail.com>
2025-06-02 08:38:09 +0000

    s2

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -46,9 +46,9 @@ export { analytics };
 // For development - connect to emulators if running locally
 if (window.location.hostname === 'localhost') {
   // Uncomment these lines if you want to use Firebase emulators for development
-  // connectFirestoreEmulator(db, 'localhost', 8080);
-  // connectAuthEmulator(auth, 'http://localhost:9099');
-  // connectStorageEmulator(storage, 'localhost', 9199);
+  connectFirestoreEmulator(db, 'localhost', 8080);
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectStorageEmulator(storage, 'localhost', 9199);
 }
 
 export default app;
